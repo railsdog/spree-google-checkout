cabbf26c05da25c59787a0b991d1d3ea206a3f73
Sean Schofield <schof@apache.org>
2009-10-17 20:23:54 -0400

    Fixed typo in address record.

--- app/controllers/google_checkout_notification_controller.rb
+++ app/controllers/google_checkout_notification_controller.rb
@@ -66,26 +66,24 @@ class GoogleCheckoutNotificationController < ApplicationController
     end
   end
  
- 
   private
-  
-    def create_spree_address_from_google_address(google_address)
-      address = Address.new
-      address.country = Country.find_by_iso(google_address.country_code)
-      address.state = State.find_by_name(google_address.region)
-      address.state_name = google_address.region if address.nil?
-      
-      address_attrs = {
-        :firstname  =>  google_address.contact_name[/^\S+/],
-        :secondname =>  google_address.contact_name[/\s.*/],
-        :address1   =>  google_address.address1, 
-        :address2   =>  google_address.address2,
-        :phone      =>  google_address.phone,
-        :zipcode    =>  google_address.postal_code
-      }
-      address.attributes = address_attrs
-      address.save(false)
-      address  
-    end
+  def create_spree_address_from_google_address(google_address)
+    address = Address.new
+    address.country = Country.find_by_iso(google_address.country_code)
+    address.state = State.find_by_name(google_address.region)
+    address.state_name = google_address.region if address.nil?
+    
+    address_attrs = {
+      :firstname  =>  google_address.contact_name[/^\S+/],
+      :lastname =>  google_address.contact_name[/\s.*/],
+      :address1   =>  google_address.address1, 
+      :address2   =>  google_address.address2,
+      :phone      =>  google_address.phone,
+      :zipcode    =>  google_address.postal_code
+    }
+    address.attributes = address_attrs
+    address.save(false)
+    address  
+  end
 
 end
